761d1e4261f567a627bf238d8cfe1e570af8280b
Joseph Gordy <jgordy2424@gmail.com>
2018-01-18 14:24:10 -0500

    successfully started server and connection to slack

--- bot.js
+++ bot.js
@@ -2,9 +2,13 @@ const RtmClient  = require('@slack/client').RtmClient;
 const WebClient  = require('@slack/client').WebClient;
 const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
-const bot_token = 'your-token-here';
+const bot_token = 'your_token_here';
 const rtm       = new RtmClient(bot_token);
 const web       = new WebClient(bot_token);
 
-rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {});
+rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+  if (message.type === 'message' && message.text) {
+    console.log(message.text);
+  }
+});
 rtm.start();
