93af0136b4e01424322a17f6cca9cdd1050ac7e5
Joseph Gordy <jgordy2424@gmail.com>
2018-01-19 11:16:10 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const WebClient  = require('@slack/client').WebClient;
 const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 var fetch = require('node-fetch');
 
-const bot_token = 'xoxb-300355127216-hlFCMLD1IRTAVS3BiLGsnBgy';
+const bot_token = 'your-token-here';
 const rtm       = new RtmClient(bot_token);
 const web       = new WebClient(bot_token);
 
