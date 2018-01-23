9691360312e65acd9d14fe06438cf5aea9a2a8b1
Joseph Gordy <jgordy2424@gmail.com>
2018-01-23 15:23:23 -0500

    commented out some code I think is throwing an error

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 const RtmClient  = require('@slack/client').RtmClient;
 const WebClient  = require('@slack/client').WebClient;
 const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
